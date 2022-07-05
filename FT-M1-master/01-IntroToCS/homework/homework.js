e672956076c5af30e496f0f4ffc57d5c669a4d25
luisruiz2000 <luisruiz462000@gmail.com>
2022-07-05 11:09:55 -0500

    01-IntroToCS

--- FT-M1-master/01-IntroToCS/homework/homework.js
+++ FT-M1-master/01-IntroToCS/homework/homework.js
@@ -2,12 +2,20 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
-
+  return num.split('').reverse().reduce(function (accumulador, elemento,index)  {
+  	  return accumulador + elemento * Math.pow(2, index);
+  	},0)
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
+  let result = ""
 
+  while (num > 0) {
+    result = (num % 2) + result;
+    num = Math.floor(num / 2);
+  }
+  return result;
 }
 
 
